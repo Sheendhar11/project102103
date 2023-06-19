bed785d2ea3673cc88d00426074984671b1029a6
Sheendhar11 <86105789+Sheendhar11@users.noreply.github.com>
2023-06-19 10:59:04 +0530

    Add files via upload

--- Move_File.py
+++ Move_File.py
@@ -1,8 +1,16 @@
+from fileinput import filename
 import os 
 import shutil
+from tabnanny import filename_only
+import time
 
 from_dir = "C:/Users/ashis/Downloads"
 to_dir = "C:/Users/ashis/Documents/Downloaded_Files"
+
+
+dir_tree = {
+    ',txt','.doc','.docx','.pdf'
+}
 
 list_Of_Files=os.listdir(from_dir)
 print(list_Of_Files)
@@ -10,3 +18,18 @@ print(list_Of_Files)
 for FileName in list_Of_Files:
     name,ext=os.path.splitext(FileName)
     print(name,ext)
+
+
+path1= from_dir+'/'+filename
+path2=to_dir+'/' + "Document_Files"
+path3=to_dir+ '/' + "Document_Files" + '/' + filename
+
+if os.path.exists(path2): 
+    print("Moving " + filename + "....") 
+    shutil.move(path1, path3)  
+else: 
+    os.makedirs(path2) 
+    print("Moving " + filename + "....") 
+    shutil.move(path1, path3) 
+                    
+    
